ce1c3c3444c35df2177f473f45858bff62544953
onyoo <roberto9m9@gmail.com>
2019-04-22 11:01:13 -0700

    Done.

--- src/components/ThemedDecorations.js
+++ src/components/ThemedDecorations.js
@@ -1,1 +1,20 @@
 // Code ThemedDecoration Component Here
+import React from 'react'
+
+
+class Invitation extends React.Component {
+
+    render() {
+        const classyChildren = React.Children.map(this.props.children, child => React.cloneElement(child, {
+            className: this.props.theme,
+        }))
+
+        return(
+            <div>
+                {classyChildren}
+            </div>
+        )
+    }
+}
+
+export default Invitation
